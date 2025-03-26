f669f7691b04c226085e6e75c78aa891745654c9
Vee Cervinski <cervinskiv26@hartfordschools.net>
2025-03-26 10:39:46 -0400

    commit message

---
+++ script.js
@@ -0,0 +1,22 @@
+const greetningName = 'World';
+console.log(greetningName)
+console.log('Hello World!');
+
+let grade = 65
+if (grade >= 90){
+    alert('A!');
+}else if (grade >= 80){
+    alert('B');
+}else if (grade >=70){
+    alert('C');
+}else {
+    alert('You Failed!'); // this will execute if none of the above conditions are met
+}
+
+let humidity = 65;
+if (humidity < 70) {
+    isRaining = false
+    if (isRaining = false){
+        alert('leave your rainjacket at home!');
+    }
+}
